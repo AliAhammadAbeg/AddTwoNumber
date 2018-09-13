5659791393f3bb0f5624dbbdce099af1033ae292
abeg42096@gmail.com <you@example.com>
2018-09-14 01:03:48 +0600

    add a console line

---
+++ add two number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add_two_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 20;
+            Console.WriteLine("substract :");
+            Console.WriteLine(a-b);
+
+            Console.ReadKey();
+        }
+    }
+}
